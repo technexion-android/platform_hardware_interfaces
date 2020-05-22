ea634ebcdd29a1e82631e27f6ee737aba8e98885
yang.tian <yang.tian@nxp.com>
2020-05-22 15:25:50 +0800

    MA-17221 BT: Can't transfer file by NXP BT
    Change BT HAL H4 write flow to send type
    and data using WriteSafely() defined in hci_protocol.cc.
    
    Use it as a workaround fix for NXP BT.
    
    Change-Id: I5186644dc6a8ef22b491df01cf7489fd3409bc93
    Signed-off-by: yang.tian <yang.tian@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/12325
    Reviewed-by: Wang Haoran <elven.wang@nxp.com>

--- bluetooth/1.0/default/h4_protocol.cc
+++ bluetooth/1.0/default/h4_protocol.cc
@@ -30,21 +30,12 @@ namespace bluetooth {
 namespace hci {
 
 size_t H4Protocol::Send(uint8_t type, const uint8_t* data, size_t length) {
-  struct iovec iov[] = {{&type, sizeof(type)},
-                        {const_cast<uint8_t*>(data), length}};
-  ssize_t ret = 0;
-  do {
-    ret =
-        TEMP_FAILURE_RETRY(writev(uart_fd_, iov, sizeof(iov) / sizeof(iov[0])));
-  } while (-1 == ret && EAGAIN == errno);
+  std::vector<uint8_t> tmp;
+  tmp.reserve(1 + length);
+  tmp.push_back(type);
+  tmp.insert(tmp.end(), data, data + length);
 
-  if (ret == -1) {
-    ALOGE("%s error writing to UART (%s)", __func__, strerror(errno));
-  } else if (ret < static_cast<ssize_t>(length + 1)) {
-    ALOGE("%s: %d / %d bytes written - something went wrong...", __func__,
-          static_cast<int>(ret), static_cast<int>(length + 1));
-  }
-  return ret;
+  return WriteSafely(uart_fd_, tmp.data(), tmp.size());
 }
 
 void H4Protocol::OnPacketReady() {
