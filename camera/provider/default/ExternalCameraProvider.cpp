53a0e7cd50d5a281aa8f41a415ab03047779754d
Fang Hui <hui.fang@nxp.com>
2023-07-19 22:26:53 +0800

    MA-20282 Fix ov5640 and hdmi-rx enabled simultaneously issure
    In order to adapt to the new dts "imx8qm-mek-hdmi-rx-ov5640.dts"
    change hdmi-rx node from mxc_isi.2 to mxc_isi.6
    
    Change-Id: Id419d54f3cae99d0eb201e1fed3ee3804c640c5e
    Signed-off-by: Fang Hui <hui.fang@nxp.com>

--- camera/provider/default/ExternalCameraProvider.cpp
+++ camera/provider/default/ExternalCameraProvider.cpp
@@ -301,7 +301,7 @@ bool ExternalCameraProvider::isExternalDevice(const char* devName, const char* s
     } else if(strstr((const char*)vidCap.driver, "cap")) {
         //HDMI RX for mek_8qm
         std::string buffer;
-        std::string propName = "mxc_isi.2.capture"; // TODO: Here is hardcoded for hdmi rx
+        std::string propName = "mxc_isi.6.capture"; // TODO: Here is hardcoded for hdmi rx
         if (!ReadFileToString(std::string(sysClassName), &buffer)) {
             ALOGE("can't read video device name");
             return false;
